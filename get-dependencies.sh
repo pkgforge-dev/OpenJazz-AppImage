0813859a2ffc49af0d37330edfb7858078acc724
Link4Electronics <link4electronics@gmail.com>
2026-02-06 11:48:18 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,31 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake    \
-    libdecor \
-    sdl2
+pacman -Syu --noconfirm libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package
+make-aur-package openjazz-git
 
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of OpenJazz..."
-echo "---------------------------------------------------------------"
-REPO="https://github.com/AlisterT/openjazz"
-VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./OpenJazz
-echo "$VERSION" > ~/version
-
-cd ./OpenJazz
-mkdir -p build && cd build
-cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DDATAPATH="/usr/share/openjazz/"
-make -j$(nproc)
-make install
