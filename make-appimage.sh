fedc9d4b84df9f32d950346f36d0dff1df0b1322
Link4Electronics <link4electronics@gmail.com>
2026-04-11 19:51:36 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,20 +3,21 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openjazz-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/OpenJazz.svg
-export DESKTOP=/usr/share/applications/OpenJazz.desktop
+export ICON=https://raw.githubusercontent.com/AlisterT/openjazz/refs/heads/master/res/unix/OpenJazz.svg
+export DESKTOP=https://raw.githubusercontent.com/AlisterT/openjazz/refs/heads/master/res/unix/OpenJazz.desktop
 export STARTUPWMCLASS=OpenJazz
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/OpenJazz
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/OpenJazz
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
