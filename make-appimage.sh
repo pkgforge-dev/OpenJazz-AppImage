e2c9aa1090d160db2c1aac409ce2fd90b8a312ad
Link4Electronics <link4electronics@gmail.com>
2026-09-04 18:44:11 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openjazz-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/OpenJazz.svg
-export DESKTOP=/usr/share/applications/OpenJazz.desktop
+export ICON=https://raw.githubusercontent.com/AlisterT/openjazz/refs/heads/master/res/unix/OpenJazz.svg
+export DESKTOP=https://raw.githubusercontent.com/AlisterT/openjazz/refs/heads/master/res/unix/OpenJazz.desktop
 export STARTUPWMCLASS=OpenJazz
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/OpenJazz
+quick-sharun ./AppDir/bin/OpenJazz
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
